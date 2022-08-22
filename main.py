a9514e6c905f471e1bff84cc9954cea2fba11fb7
Gerardo Gutiérrez <33466934+GerardoGtz25@users.noreply.github.com>
2022-08-22 15:45:33 -0500

    Model Person

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
+from typing import Optional
+from pydantic import BaseModel
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
+
+#Models
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
 
 # Path Parameters
 # Query Parameters
 # "/tweets/{tweet_id}/details?age=30&height=184"
-@app.get("/")
+@app.get('/')
 def home():
-    message = "Hola Mundo"
+    message = 'Hola Mundo'
     print(message)
-    return {"message": message}
+    return {'message': message}
+
+
+#Request and reponse body
+@app.post('/persons')
+def create_person(person: Person = Body(...)):
+
+    return person
